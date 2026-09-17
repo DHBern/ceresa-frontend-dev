1dcac6a3c7e4df2a915df9451c248ca942b279c8
vvvyyynet <levyn.buerki@unibe.ch>
2026-09-17 22:06:07 +0200

    del: remove obsolete smallformsLongforms route

--- src/params/smallformsLongforms.ts
+++
@@ -1,7 +0,0 @@
-import type { ParamMatcher } from '@sveltejs/kit';
-
-const docTypeIds = ['smallforms', 'longforms'];
-
-export const match = ((param: string) => {
-	return docTypeIds.includes(param);
-}) satisfies ParamMatcher;
