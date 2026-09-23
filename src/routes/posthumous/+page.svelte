4d9655aaa7bd5a0a0589b26f10b6b8c8eff6d0cd
vvvyyynet <levyn.buerki@unibe.ch>
2026-09-23 21:56:40 +0200

    add routes for posthumous, unpublished and published

---
+++ src/routes/posthumous/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { doc_sequences as seqAll } from '$lib/data/doc_sequences.json';
+	import type { TSeqSmallformsKeys } from '$lib/types/TSequences';
+	import { resolve } from '$app/paths';
+	import { dict_docs as dictDoc } from '$lib/dictionaries/dict_docs.json';
+	import DocumentsNav from '$lib/components/DocumentsNav.svelte';
+
+	let seqData = $derived(seqAll.smallforms);
+</script>
+
+<!-- Series -->
+{#snippet keyList(keys: TSeqSmallformsKeys[])}
+	<div class={['preset-btn-list items-center justify-center', '--spacing-sm']}>
+		{#each keys as key (key)}
+			<a class="preset-btn-round --normal" href={resolve(`/${key}` as any)}
+				>{seqData[key as TSeqSmallformsKeys].name}</a
+			>
+		{/each}
+	</div>
+{/snippet}
+
+<div class={['z-100 mx-auto flex max-w-300 flex-col gap-10 bg-background px-10']}>
+	<!-- Navigation -->
+	<DocumentsNav docType="smallforms" />
+	<div class={['flex gap-10 py-5']}>
+		<!-- Posthumous -->
+		<div class="flex flex-col gap-7">
+			<h1 class={['h1 text-center whitespace-nowrap transition-all duration-200']}>
+				{dictDoc['posthumous']?.label_plural}
+			</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+				laboris nisi ut aliquip ex ea commodo consequat.
+			</p>
+			<div class="flex flex-wrap justify-center gap-2">
+				{@render keyList(
+					Object.keys(seqData).filter((key) => {
+						return (key as string) === 'posthumous_all';
+					}) as TSeqSmallformsKeys[]
+				)}
+			</div>
+		</div>
+	</div>
+</div>
--- src/routes/texts_and_audio/+page.svelte
+++ src/routes/texts_and_audio/+page.svelte
@@ -6,17 +6,6 @@
 	let seqData = $derived(seqAll.smallforms);
 </script>
 
-<!-- Series -->
-{#snippet keyList(keys: TSeqSmallformsKeys[])}
-	<div class={['preset-btn-list items-center justify-center', '--spacing-sm']}>
-		{#each keys as key (key)}
-			<a class="preset-btn-round --normal" href={resolve(`/${key}` as any)}
-				>{seqData[key as TSeqSmallformsKeys].name}</a
-			>
-		{/each}
-	</div>
-{/snippet}
-
 <div class={['z-100 mx-auto flex max-w-300 flex-col gap-10 bg-background px-10']}>
 	<!-- Navigation -->
 	<h1 class={['h1 text-center whitespace-nowrap transition-all duration-200']}>Texte und Audio</h1>
@@ -32,52 +21,40 @@
 	<div class={['grid grid-cols-3 gap-10 py-5']}>
 		<!-- Posthumous -->
 		<div class="flex flex-col gap-7">
-			<h2 class="h2 text-center">Posthumous</h2>
+			<h2 class="h2 text-center">Posthumous Works</h2>
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
 				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
 				laboris nisi ut aliquip ex ea commodo consequat.
 			</p>
 			<div class="flex flex-wrap justify-center gap-2">
-				{@render keyList(
-					Object.keys(seqData).filter((key) => {
-						return (key as string) === 'smallforms_all';
-					}) as TSeqSmallformsKeys[]
-				)}
+				<a class="preset-btn-round --normal" href={resolve('/posthumous')}>Posthumous Works</a>
 			</div>
 		</div>
 
 		<!-- Unpublished -->
 		<div class="flex flex-col gap-7">
-			<h2 class="h2 text-center">Unpublished</h2>
+			<h2 class="h2 text-center">Unpublished Works</h2>
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
 				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
 				laboris nisi ut aliquip ex ea commodo consequat.
 			</p>
 			<div class="flex flex-wrap justify-center gap-2">
-				{@render keyList(
-					Object.keys(seqData).filter((key) => {
-						return (key as string) === 'smallforms_all';
-					}) as TSeqSmallformsKeys[]
-				)}
+				<a class="preset-btn-round --normal" href={resolve('/unpublished')}>Unpublished Works</a>
 			</div>
 		</div>
 
 		<!-- Published -->
 		<div class="flex flex-col gap-7">
-			<h2 class="h2 text-center">Published</h2>
+			<h2 class="h2 text-center">Published Works</h2>
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
 				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
 				laboris nisi ut aliquip ex ea commodo consequat.
 			</p>
 			<div class="flex flex-wrap justify-center gap-2">
-				{@render keyList(
-					Object.keys(seqData).filter((key) => {
-						return (key as string) === 'smallforms_all';
-					}) as TSeqSmallformsKeys[]
-				)}
+				<a class="preset-btn-round --normal" href={resolve('/published')}>Published Works</a>
 			</div>
 		</div>
 	</div>
